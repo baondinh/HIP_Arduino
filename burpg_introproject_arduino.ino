0912712a3db89fd1f40f37fc1b9f584a516cae1d
Bao Dinh <49595165+baondinh@users.noreply.github.com>
2024-10-24 12:45:24 -0400

    Update Arduino setup

--- burpg_introproject_arduino.ino
+++ burpg_introproject_arduino.ino
@@ -1,7 +1,11 @@
 void setup(){
-
+  // Setup code run only once
+  // Open serial port and set data rate to 9600 bits per second
+  Serial.begin(9600); 
+  Serial.println("Arduino Setup Test");
+  delay(2000); //delay for 2000 milliseconds or 2 seconds
 }
 
 void loop() {
-
+  // Main code run repeatedly
 } 
