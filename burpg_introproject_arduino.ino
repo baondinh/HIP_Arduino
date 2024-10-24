27c61de92030d5ef47985c59aaeec104979cc97f
Bao Dinh <49595165+baondinh@users.noreply.github.com>
2024-10-24 13:19:52 -0400

    Comment overall idea for project in loop function

--- burpg_introproject_arduino.ino
+++ burpg_introproject_arduino.ino
@@ -1,3 +1,6 @@
+// Variables 
+button_state = 0; // Use input 
+
 void setup(){
   // Setup code run only once
   // Open serial port and set data rate to 9600 bits per second
@@ -8,4 +11,19 @@ void setup(){
 
 void loop() {
   // Main code run repeatedly
+  /* 
+  1. Sample load cell every 0.5s (ignition should be ~3s, so 0.5s allows for multiple data points before, during, and after ignition)
+  2. Check if input button state high (button is pressed --> state == 1) 
+  3. If button_state == 1, then: 
+      Open solenoid
+      Delay xs
+      Trigger ematch spark (one time use but for good practice, turn off signal to spark) 
+      Wait at least 3s to sample ignition 
+      Close solenoid 
+
+  Notes: 
+  How to handle multiple button presses within a short time-span (how to handle someone mashing the button and prevent conflicting signals) 
+  */ 
+  delay(500) //0.5s delay for sampling
+  // if 
 } 
